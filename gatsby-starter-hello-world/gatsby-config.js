ac08db08cfaf176843d33666fc63885e06677eab
James Fincher <heartandkarma@gmail.com>
2022-05-29 22:04:43 -0700

    added source filesystem plugin pointing to src/assets

--- gatsby-starter-hello-world/gatsby-config.js
+++ gatsby-starter-hello-world/gatsby-config.js
@@ -11,5 +11,20 @@ module.exports = {
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/assets/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
   ],
 }
